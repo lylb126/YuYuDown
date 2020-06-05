62e3f366da32b3ce4df4c697cf3383ae2c98c971
扬 刘 <630002432@qq.com>
2020-06-05 17:07:41 +0800

    增加称呼可从App.config配置

--- StructCode/ErrorCode.cs
+++ StructCode/ErrorCode.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 
@@ -11,17 +12,22 @@ namespace YuYuDown
     public struct ErrorCode
     {
         /// <summary>
-        /// 煜煜,报错了联系老刘吧
-        /// </summary>
-        public static string ErrorMsg => "煜煜,报错了联系老刘吧";
-        /// <summary>
         /// 老刘提示
         /// </summary>
         public static string Caption => "老刘提示!!";
         /// <summary>
+        ///  前置
+        /// </summary>
+        public static string Callstr => ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None).AppSettings
+            .Settings["CallMsg"].Value;
+        /// <summary>
+        /// 煜煜,报错了联系老刘吧
+        /// </summary>
+        public static string ErrorMsg => $"{Callstr},报错了联系老刘吧";
+        /// <summary>
         /// 煜煜,下载出错啦!找老刘吧!
         /// </summary>
-        public static string NetWorkError => "煜煜,你看看是不是没联网!";
+        public static string NetWorkError => $"{Callstr},你看看是不是没联网!";
         /// <summary>
         /// 成功收到数据
         /// </summary>
@@ -29,35 +35,35 @@ namespace YuYuDown
         /// <summary>
         /// 煜煜,没有找到这个动漫
         /// </summary>
-        public static string SelectNull => "煜煜,没有找到这个动漫";
+        public static string SelectNull => $"{Callstr},没有找到这个动漫";
         /// <summary>
         /// 煜煜,查询出错了
         /// </summary>
-        public static string SelectError => "煜煜,查询出错了";
+        public static string SelectError => $"{Callstr},查询出错了";
         /// <summary>
         /// 煜煜,报错了联系老刘吧
         /// </summary>
-        public static string IdNullMsg => "煜煜,报错了联系老刘吧";
+        public static string IdNullMsg => $"{Callstr},报错了联系老刘吧";
         /// <summary>
         /// 这个目录还没有被创建！你直接开始下载就好了
         /// </summary>
-        public static string FolderPathNull => "这个目录还没有被创建！你直接开始下载就好了";
+        public static string FolderPathNull => $"{Callstr},这个目录还没有被创建！你直接开始下载就好了";
         /// <summary>
         /// 煜煜,下载完成了
         /// </summary>
-        public static string DownSuccess => "煜煜,下载完成了";
+        public static string DownSuccess => $"{Callstr},下载完成了";
         /// <summary>
         /// 煜煜,下载出错啦
         /// </summary>
-        public static string DownError => "煜煜,下载出错啦";
+        public static string DownError => $"{Callstr},下载出错啦";
         /// <summary>
         /// 煜煜,下载图片出错啦。找老刘吧
         /// </summary>
-        public static string DownImgError => "煜煜,下载图片出错啦。找老刘吧";
+        public static string DownImgError => $"{Callstr},下载图片出错啦。找老刘吧";
         /// <summary>
         /// 煜煜,下载出错啦!找老刘吧!
         /// </summary>
-        public static string DownActionError => "煜煜,下载出错啦!找老刘吧!";
+        public static string DownActionError => $"{Callstr},下载出错啦!找老刘吧!";
 
     } 
 
