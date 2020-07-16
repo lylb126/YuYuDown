9937d5cc9415f70bbf0843a22523f908a0c426cb
扬 刘 <630002432@qq.com>
2020-07-16 11:10:50 +0800

    深度复制

---
+++ Common/CloneExtends.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace YuYuDown.Common
+{
+    /// <summary>
+    ///  深度拷贝
+    ///     JS序列化也可以实现深度拷贝
+    /// </summary>
+    public static class  CloneExtends
+    {
+        public static T DeepCloneObject<T>(this T t) where T : class
+        {
+            T model = System.Activator.CreateInstance<T>();                     //实例化一个T类型对象
+            PropertyInfo[] propertyInfos = model.GetType().GetProperties();     //获取T对象的所有公共属性
+            foreach (PropertyInfo propertyInfo in propertyInfos)
+            {
+                //判断值是否为空，如果空赋值为null见else
+                if (propertyInfo.PropertyType.IsGenericType && propertyInfo.PropertyType.GetGenericTypeDefinition().Equals(typeof(Nullable<>)))
+                {
+                    //如果convertsionType为nullable类，声明一个NullableConverter类，该类提供从Nullable类到基础基元类型的转换
+                    NullableConverter nullableConverter = new NullableConverter(propertyInfo.PropertyType);
+                    //将convertsionType转换为nullable对的基础基元类型
+                    propertyInfo.SetValue(model, Convert.ChangeType(value: propertyInfo.GetValue(t, null), conversionType: nullableConverter.UnderlyingType), null);
+                }
+                else
+                {
+                    propertyInfo.SetValue(model, Convert.ChangeType(propertyInfo.GetValue(t,null), propertyInfo.PropertyType), null);
+                }
+            }
+            return model;
+        }
+        public static IList<T> DeepCloneList<T>(this IList<T> tList) where T : class
+        {
+            IList<T> listNew = new List<T>();
+            foreach (var item in tList)
+            {
+                T model = System.Activator.CreateInstance<T>();                     //实例化一个T类型对象
+                PropertyInfo[] propertyInfos = model.GetType().GetProperties();     //获取T对象的所有公共属性
+                foreach (PropertyInfo propertyInfo in propertyInfos)
+                {
+                    //判断值是否为空，如果空赋值为null见else
+                    if (propertyInfo.PropertyType.IsGenericType && propertyInfo.PropertyType.GetGenericTypeDefinition().Equals(typeof(Nullable<>)))
+                    {
+                        //如果convertsionType为nullable类，声明一个NullableConverter类，该类提供从Nullable类到基础基元类型的转换
+                        NullableConverter nullableConverter = new NullableConverter(propertyInfo.PropertyType);
+                        //将convertsionType转换为nullable对的基础基元类型
+                        propertyInfo.SetValue(model, Convert.ChangeType(propertyInfo.GetValue(item, null), nullableConverter.UnderlyingType), null);
+                    }
+                    else
+                    {
+                        propertyInfo.SetValue(model, Convert.ChangeType(propertyInfo.GetValue(item, null), propertyInfo.PropertyType), null);
+                    }
+                }
+                listNew.Add(model);
+            }
+            return listNew;
+        }
+        /// <summary>
+        /// 深拷贝对象
+        /// </summary>
+        /// <typeparam name="Model">拷贝对象类型</typeparam>
+        /// <param name="instance">被拷贝的实例</param>
+        /// <returns>成功返回新的实例；否则返回null</returns>
+        public static T DeepClone<T>(T instance) where T : class
+        {
+            //          JSON序列化来做深拷贝处理
+            if (instance == null) return null;
+            String jsonString = JsonConvert.SerializeObject(instance);
+            return JsonConvert.DeserializeObject<T>(jsonString);
+        }
+    }
+}
